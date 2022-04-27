d3dec818213962279093d65e09c16e262c46d7c3
MarcCrusellas <71900639+MarcCrusellas@users.noreply.github.com>
2022-04-27 15:53:46 +0200

    frh

--- app/src/main/java/com/neuro/simplev6/ui/home/HomeFragment.java
+++ app/src/main/java/com/neuro/simplev6/ui/home/HomeFragment.java
@@ -19,11 +19,9 @@ public class HomeFragment extends Fragment {
 
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
 
 
-
-        return root;
+        return binding.getRoot();
     }
 
     @Override
